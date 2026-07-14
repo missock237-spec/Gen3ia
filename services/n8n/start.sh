6d421106c79368724b1cce99795d417d97527943
missock237-spec <259838606+missock237-spec@users.noreply.github.com>
2026-07-14 07:00:18 +0100

    chore: remove n8n service permanently

--- services/n8n/start.sh
+++ services/n8n/start.sh
@@ -1,14 +1,1 @@
-#!/bin/bash
-# n8n Workflow Engine for Genova Genova
-# Runs on port 5678 with data stored locally
-
-export N8N_PORT=5678
-export N8N_PROTOCOL=http
-export N8N_HOST=localhost
-export N8N_EDITOR_BASE_URL=http://localhost:5678
-export N8N_USER_FOLDER=/home/z/my-project/services/n8n
-export N8N_CUSTOM_EXTENSIONS=/home/z/my-project/services/n8n/extensions
-export WEBHOOK_URL=http://localhost:5678/
-
-cd /home/z/my-project
-npx n8n start
+# REMOVED - n8n service deleted
