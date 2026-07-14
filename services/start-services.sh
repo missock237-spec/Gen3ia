4252fe8e1870d2162055302c40ada6b5e934d2f9
missock237-spec <259838606+missock237-spec@users.noreply.github.com>
2026-07-14 07:00:55 +0100

    chore: remove n8n from start-services.sh

--- services/start-services.sh
+++ services/start-services.sh
@@ -32,17 +32,7 @@ start_detached "pocketbase" "/home/z/my-project/services/pocketbase/pocketbase s
 
 sleep 1
 
-# 4. n8n
-export N8N_BASIC_AUTH_ACTIVE=true
-export N8N_BASIC_AUTH_USER=admin
-export N8N_BASIC_AUTH_PASSWORD=genova_admin
-export N8N_PORT=5678
-export WEBHOOK_URL=http://localhost:5678/
-start_detached "n8n" "/home/z/.npm-global/bin/n8n start" "/home/z/my-project/services"
-
-sleep 1
-
-# 5. SpeechBrain
+# 4. SpeechBrain
 start_detached "speechbrain" "python3 /home/z/my-project/services/speechbrain_api_server.py" "/home/z/my-project/services"
 
 echo "All services started"
