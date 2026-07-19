ac233b326f621ffb062bbc2dd1836847b1c4a1f2
missock237 <missock237@gmail.com>
2026-07-19 13:38:02 +0100

    cleanup: suppression fichiers inutiles, dossiers orphelins, package-lock.json

--- services/pocketbase/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/z/my-project/services/pocketbase
-./pocketbase serve --http=0.0.0.0:8090 &
-echo "PocketBase started on http://localhost:8090"
-echo "Admin UI: http://localhost:8090/_/"
