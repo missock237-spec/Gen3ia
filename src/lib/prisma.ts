34acd86d921ad5caab730d7490eb7ac3c50fb493
missock237-spec <missock237@gmail.com>
2026-07-24 15:11:44 +0100

    feat: prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,8 @@
 import { PrismaClient } from '@prisma/client';
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined };
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({
-  datasource: {
-    url: process.env.DATABASE_URL,
-  },
-  log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+  datasourceUrl: process.env.DATABASE_URL,
+  log: process.env.NODE_ENV === 'development' ? ['warn', 'error'] : ['error'],
 });
-
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
-
 export default prisma;
